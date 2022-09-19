f76af89e033df2556cff8c768e34729c9b56b526
Sultan Omar <sultanomar756@gmail.com>
2022-09-19 17:13:37 +0600

    category router

---
+++ controllers/categoryController.js
@@ -0,0 +1,10 @@
+const _ = require('lodash');
+const {} = require('../models/category');
+
+module.exports.createCategory = async (req, res) =>{
+
+}
+
+module.exports.getCategories = async (req, res) => {
+    
+}
