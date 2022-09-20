abb2adb0d8f5b1b284aedb1f683f2db36c94ce18
Sultan Omar <sultanomar756@gmail.com>
2022-09-20 09:33:09 +0600

    check 1

---
+++ routers/productRouter.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const {getProductById, createProduct, 
+    getProducts, updateProductById} = require('../controllers/productController');
+const admin = require('../middlewares/admin');
+const authorize = require('../middlewares/authorize');
+
+
+
+router.route('/')
+.post([authorize, admin], createProduct)
+.get(getProducts);
+
+router.route('/:id')
+.get(getProductById)
+.put(updateProductById)
+
+module.exports= router;
